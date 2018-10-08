c9d92d592d1bb6151363ae34c3efdd7b9d7ae729
Timmo404 <cala.stephane@gmail.com>
2018-10-08 17:47:16 +0200

    exo 04

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
-#write your code here
+def echo(a)#write your code here
+a = a.to_s
+end
+
+def shout(a)
+a.upcase
+end
+
+def repeat(a,b=2)
+  a += ( " " + a ) * ( b - 1 )
+end
+
+def start_of_word( a,b=0)
+    a[0..b-1]
+end
+
+def first_word(a)
+a.split.first
+end
+
+def titleize(title)
+  title = title.split(" ").map {|word| word.capitalize}.join(" ")
+	title = title.gsub("And", "and")
+	title = title.gsub("To", "to")
+	title = title.gsub("The", "the")
+	title = title.gsub("but", "but")
+	title[0] = title[0].upcase
+	return title
+end
