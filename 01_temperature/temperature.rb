fa7287a675294da2551192bc2a4c09bb1f455d3f
Timmo404 <cala.stephane@gmail.com>
2018-10-08 14:19:44 +0200

    deux exo en plus 01 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
 #write your code here
+
+
+
+def ftoc(far)
+far = (far - 32)* 5 / 9
+end
+
+def ctof(celsius)
+celsius = celsius * 9.0 / 5.0 + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(num1,num2)
+  num1 + num2
+end
+
+def subtract(num1,num2)
+  num1 - num2
+end
+
+nb = Array.new
+nb = [1,3,5,7,9,11]
+
+ def sum(arr)
+   arr.inject(0){|sum,x| sum + x }
+ end
+def multiply(num1,num2)
+  num1 * num2
+end
+
+def power(num1, num2)
+  num1**num2
+end
+
+#ne marche pas
+def fact(num1)
+res = 1
+  while num1 > 0
+    res *= num1
+    num1 -= 1
+end
+return res
+end
